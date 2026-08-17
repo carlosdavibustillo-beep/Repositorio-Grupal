9bba8aa2f50f269e92f1e4afeacf67474f8d5e02
carlosdavibustillo-beep <carlosdavibustillo@gmail.com>
2026-08-17 16:20:28 -0600

    Update 04_agregaciones_joins.sql

--- sql/04_agregaciones_joins.sql
+++ sql/04_agregaciones_joins.sql
@@ -1,8 +1,15 @@
--- 1. Listar todos los productos con precio mayor a L. 100
-SELECT * FROM productos WHERE precio > 100.00;
+-- 1. Obtener listado de productos con el nombre de su categoría (JOIN)
+SELECT p.nombre AS producto, p.precio, c.nombre AS categoria
+FROM productos p
+INNER JOIN categorias c ON p.id_categoria = c.id;
 
--- 2. Consultar clientes registrados con sus RTN
-SELECT nombre, rtn FROM clientes;
+-- 2. Consultar facturas con el nombre del cliente que compró
+SELECT f.id AS num_factura, f.fecha, cl.nombre AS cliente
+FROM facturas f
+INNER JOIN clientes cl ON f.id_cliente = cl.id;
 
--- 3. Ver productos con stock bajo (menos de 30 unidades)
-SELECT nombre, stock FROM productos WHERE stock < 30 ORDER BY stock ASC;
+-- 3. Calcular el total de stock disponible por categoría (GROUP BY / Agregación)
+SELECT c.nombre AS categoria, SUM(p.stock) AS total_stock
+FROM productos p
+INNER JOIN categorias c ON p.id_categoria = c.id
+GROUP BY c.nombre;
