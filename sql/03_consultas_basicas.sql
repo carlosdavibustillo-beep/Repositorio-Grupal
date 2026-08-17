5e8ecdd498a5f0b80583d832a4538d4aac90910b
alefunezlopez-ops <alefunezlopez@gmail.com>
2026-08-17 16:49:01 -0600

    Update 03_consultas_basicas.sql

--- sql/03_consultas_basicas.sql
+++ sql/03_consultas_basicas.sql
@@ -2,3 +2,5 @@
 SELECT * FROM productos WHERE precio > 100.00;
 -- 2. Consultar clientes registrados con sus RTN
 SELECT nombre, rtn FROM clientes;
+-- 3. Ver productos con stock bajo (menos de 30 unidades)
+SELECT nombre, stock FROM productos WHERE stock < 30 ORDER BY stock ASC;
