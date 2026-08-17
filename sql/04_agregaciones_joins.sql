f2e430ae12d4e581e19204268e1837e0605cfdf1
carlosdavibustillo-beep <carlosdavibustillo@gmail.com>
2026-08-17 15:52:52 -0600

    Update 04_agregaciones_joins.sql

--- sql/04_agregaciones_joins.sql
+++ sql/04_agregaciones_joins.sql
@@ -1,1 +1,8 @@
+-- 1. Listar todos los productos con precio mayor a L. 100
+SELECT * FROM productos WHERE precio > 100.00;
 
+-- 2. Consultar clientes registrados con sus RTN
+SELECT nombre, rtn FROM clientes;
+
+-- 3. Ver productos con stock bajo (menos de 30 unidades)
+SELECT nombre, stock FROM productos WHERE stock < 30 ORDER BY stock ASC;
